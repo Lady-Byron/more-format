5abedac4844396c493eb55551c2b3a3672d14e42
Lady-Byron <samantha-everett@outlook.com>
2025-08-18 16:44:10 -0400

    Update webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,2 +1,9 @@
-const config = require('flarum-webpack-config');
-module.exports = config();
+const path = require('path');
+const base = require('flarum-webpack-config')();
+
+// 显式指定入口，避免自动探测不到 .tsx 时生成 0 个入口
+base.entry = {
+  forum: path.resolve(__dirname, 'src/forum/index.tsx'),
+};
+
+module.exports = base;
