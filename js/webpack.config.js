ce7f66ee2aa2d8b00e85bec489156e356dd86c39
Lady-Byron <samantha-everett@outlook.com>
2025-09-04 18:34:27 -0400

    Update webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,11 +1,8 @@
 const path = require('path');
+const base = require('flarum-webpack-config')();
 
-// 告诉 flarum-webpack-config：此前端会使用到其它扩展的导出
-module.exports = require('flarum-webpack-config')({
-  useExtensions: ['askvortsov-rich-text'],
-});
-
-module.exports.entry = {
-  forum: path.resolve(__dirname, 'src/forum/index.tsx'),
+base.entry = {
+  forum: path.resolve(__dirname, 'src/forum/index.ts'),
 };
 
+module.exports = base;
