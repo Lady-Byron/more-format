4940a00925c04a0dbfa2ed0c6f44e20f2a980175
Lady-Byron <samantha-everett@outlook.com>
2025-09-04 16:46:35 -0400

    Update webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const base = require('flarum-webpack-config')();
 
 base.entry = {
-  forum: path.resolve(__dirname, 'src/forum/index.ts'),
+  forum: path.resolve(__dirname, 'src/forum/index.tsx'),
 };
 
 module.exports = base;
